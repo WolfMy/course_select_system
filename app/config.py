eb46b14a58c66b64881d4c348ee7a094c0e58603
mouyu <mouyu@bytedance.com>
2026-01-07 14:55:22 +0800

    refactor: update database configuration and fix typos in student model
    - Update database URI to remove password
    - Fix typo in StudentPassword field name
    - Update Flask and related dependencies to newer versions
    - Improve migrations configuration
    - Update ProxyFix import path

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess!'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/course_select_system?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/course_select_system?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app import app
 
 if __name__ == "__main__":
-    from werkzeug.contrib.fixers import ProxyFix
+    from werkzeug.middleware.proxy_fix import ProxyFix
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(host='0.0.0.0', port=4070, debug=False)
